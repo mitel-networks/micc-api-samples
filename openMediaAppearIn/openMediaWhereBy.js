d6125fa1cbb14fad22a3e20115ff545eea8dea55
shaapala <shaun.haapala@mitel.com>
2020-03-31 09:45:51 -0400

    Changed all references of appear.in to whereby.com and updated readme as well (#14)

---
+++ openMediaAppearIn/openMediaWhereBy.js
@@ -0,0 +1,51 @@
+var micc = new Micc('localhost');
+
+function toggleAccordion(elementId) {
+    var accordian = document.getElementById(elementId);
+    const showClass = ' w3-show';
+    if(accordian.className.indexOf(showClass) == -1) {
+        accordian.className += showClass;
+        } else {
+        accordian.className = accordian.className.replace(showClass, '');
+    }
+}
+
+function login() {
+    micc = new Micc($('#miccServer').val());
+    micc.login(
+    $('#username').val(),
+    $('#password').val(), 
+    function(responseData) {
+        //you can write code here to process the data from the login
+        //Remember to not to expose the access token here
+    }
+    );
+}
+
+function postOpenMedia() {
+    
+    var id = generateGuid();
+    var targetUri = `https://whereby.com/${id}`;
+    
+    micc.postOpenMedia(`{
+        "targetUri": '${targetUri}',
+        "targetUriEmbedded": "true",
+        "previewUrl": "",
+        "id": "${id}",
+        "historyUrl": "https://whereby.com/",
+        "from": "Random Kiosk User",
+        "to": "Company X",
+        "subject": "Sample Open Media App"
+        }`, function(responseData) {
+        console.log('Response from POST open media:  ', responseData);
+        
+        window.location = `${targetUri}`;
+    });
+}
+
+function generateGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
--- openMediaAppearIn/readme.md
+++ openMediaAppearIn/readme.md
@@ -1,10 +1,11 @@
-# openMediaAppearIn.html
-Basic demo of open media conversation routing through integration with [appear.in](https://appear.in/)
+# openMediaWhereBy.html
+
+Basic demo of open media conversation routing through integration with [whereby.com](https://whereby.com/).
+Note: you will need to setup a free accoun on whereby.com before proceeding.
 
 * All errors are written to the console.
 * Login first with username and password.
 * Post login - click on the start chat button.
-* An open media conversation is routed to an agent with targetUri = https://appear.in/${id}
-* If the conversation is created successfully, the window should navigate to https://appear.in/${id} page - this means you have created and joined a video conferencing room on appear.in
-* When the agent accepts the conversation the agent would join the same room i.e. https://appear.in/${id}. The two sides (customer and agent) could now communicate.
-
+* An open media conversation is routed to an agent with targetUri = https://whereby.com/${id}
+* If the conversation is created successfully, the window should navigate to https://whereby.com/${id} page - this means you have created and joined a video conferencing room on whereby.com
+* When the agent accepts the conversation the agent would join the same room i.e. https://whereby.com/${id}. The two sides (customer and agent) could now communicate.
